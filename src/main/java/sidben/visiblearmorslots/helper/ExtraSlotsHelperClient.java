2a461a11ba7614aa119ef06b4966d8badd6c3d0c
sidben <sidbenetti@gmail.com>
2017-01-15 15:39:21 -0200

    Tweak so the mod can build

--- src/main/java/sidben/visiblearmorslots/helper/ExtraSlotsHelperClient.java
+++ src/main/java/sidben/visiblearmorslots/helper/ExtraSlotsHelperClient.java
@@ -60,7 +60,10 @@ public class ExtraSlotsHelperClient extends ExtraSlotsHelperCommon
         try {
             xSize = FIELD_XSIZE.getInt(originalGui);
             ySize = FIELD_YSIZE.getInt(originalGui);
-        } catch (IllegalArgumentException | IllegalAccessException e) {
+        } catch (IllegalArgumentException e) {
+            LogHelper.info("Error loading xSize and ySize from container.");
+            LogHelper.error(e);
+        } catch (IllegalAccessException e) {
             LogHelper.info("Error loading xSize and ySize from container.");
             LogHelper.error(e);
         }
