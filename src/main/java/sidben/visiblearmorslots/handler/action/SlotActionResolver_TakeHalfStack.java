5fb231f0a30f8bd9c6a0bceca9b8238340c1af78
sidben <sidbenetti@gmail.com>
2017-02-05 15:04:00 -0200

    Implemented SlotActionResolver_TakeHalfStack

--- src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TakeHalfStack.java
+++ src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TakeHalfStack.java
@@ -1,15 +1,58 @@
 package sidben.visiblearmorslots.handler.action;
 
-import sidben.visiblearmorslots.helper.LogHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 
+/**
+ * Takes half of the items in the target slot.
+ */
 public class SlotActionResolver_TakeHalfStack extends SlotActionResolver
 {
 
     @Override
+    public void handleClientSide(Slot targetSlot, EntityPlayer player)
+    {
+        this.takeHalfStack(targetSlot, player);
+    }
+
+
+    @Override
+    public void handleServerSide(Slot targetSlot, EntityPlayer player)
+    {
+        this.takeHalfStack(targetSlot, player);
+    }
+
+
+    /**
+     * Reference: {@link net.minecraft.inventory.Container#slotClick() Container.slotClick()}
+     */
+    private void takeHalfStack(Slot targetSlot, EntityPlayer player)
+    {
+        final int amount = (targetSlot.getStack().getCount() + 1) / 2;
+
+        final ItemStack slotStack = targetSlot.getStack();
+        player.inventory.setItemStack(targetSlot.decrStackSize(amount));
+        if (slotStack.isEmpty()) {
+            targetSlot.putStack(ItemStack.EMPTY);
+        }
+        targetSlot.onTake(player, player.inventory.getItemStack());
+    }
+
+
+
+    @Override
+    public boolean requiresServerSideHandling()
+    {
+        return true;
+    }
+
+
+    @Override
     protected boolean isSatisfiedByInternal(SlotActionType action)
     {
-        if (action.mouseButton.equals(SlotActionType.MouseButton.PLACE_BLOCK_BUTTON) && action.slotHasItemStack) { return true; }
+        if (action.mouseButton.equals(SlotActionType.MouseButton.PLACE_BLOCK_BUTTON) && action.slotHasItemStack && !action.playerMouseHasItemStack) { return true; }
         return false;
     }
 
