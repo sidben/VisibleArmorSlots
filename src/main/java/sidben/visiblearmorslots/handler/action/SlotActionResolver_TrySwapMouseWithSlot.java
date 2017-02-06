96f5185ed5deae541571753e07a182a2135aaeb6
sidben <sidbenetti@gmail.com>
2017-02-06 01:14:29 -0200

    Allowed SlotActionResolver_TrySwapMouseWithSlot to combine stacks (off-hand slot)

--- src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TrySwapMouseWithSlot.java
+++ src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TrySwapMouseWithSlot.java
@@ -4,7 +4,9 @@ import java.util.EnumSet;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import sidben.visiblearmorslots.handler.action.SlotActionType.MouseButton;
+import sidben.visiblearmorslots.util.ItemStackHelper;
 
 
 // TODO: Future config: can place any item on head slot
@@ -42,14 +44,22 @@ public class SlotActionResolver_TrySwapMouseWithSlot extends SlotActionResolver
 
         final boolean canPlaceOnSlot = playerMouseItem.isEmpty() || targetSlot.isItemValid(playerMouseItem);
         final boolean canTakeFromSlot = targetSlot.getStack().isEmpty() || targetSlot.canTakeStack(player);
+        final boolean stacksCompatible = ItemStackHelper.areStacksCompatible(playerMouseItem, targetSlot.getStack());
 
-        if (canPlaceOnSlot && canTakeFromSlot) {
+        if (stacksCompatible && targetSlot.getStack().isStackable()) {
+            // Combines the stacks
+            final int amountTheSlotCanTake = Math.max(targetSlot.getStack().getMaxStackSize() - targetSlot.getStack().getCount(), 0);
+            final int amountTheSlotWillTake = MathHelper.clamp(playerMouseItem.getCount(), 0, amountTheSlotCanTake);
+            targetSlot.getStack().grow(amountTheSlotWillTake);
+            playerMouseItem.shrink(amountTheSlotWillTake);
+            targetSlot.onSlotChanged();
+            
+        } else if (canPlaceOnSlot && canTakeFromSlot) {
             // Swaps the item on the player mouse with the target slot
             player.inventory.setItemStack(targetSlot.getStack());
             targetSlot.putStack(playerMouseItem);
+            
         }
-
-        // TODO: combine stacks
     }
 
 
