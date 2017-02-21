5716f54cf758873a3164e625072b36ddaa42eca9
sidben <sidbenetti@gmail.com>
2017-02-21 01:25:03 -0300

    Refactored SlotActionResolver_TrySwapWithOffHandSlot to make it behave more like vanilla.
    This was an attempt to fix a bug, but turns out it was just an incompatibility with the Quark mod, that adds a similar feature.

--- src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TrySwapWithOffHandSlot.java
+++ src/main/java/sidben/visiblearmorslots/handler/action/SlotActionResolver_TrySwapWithOffHandSlot.java
@@ -15,8 +15,10 @@ public class SlotActionResolver_TrySwapWithOffHandSlot extends SlotActionResolve
 {
 
     private final String CREATIVE_CONTAINER_NAME = "ContainerCreative";
+    private final int    OFF_HAND_SLOT_INDEX     = 40;
 
     private boolean      _needsServerSide        = false;
+
 
 
     @Override
@@ -39,54 +41,82 @@ public class SlotActionResolver_TrySwapWithOffHandSlot extends SlotActionResolve
     {
         if (targetSlot instanceof SlotOffHand) { return; }
 
-        final Slot offHandSlot = player.inventoryContainer.getSlot(45);
-        final ItemStack offHandStack = offHandSlot.getStack();
-        final boolean canPlaceOnSlot = offHandStack.isEmpty() || targetSlot.isItemValid(offHandStack);
-        final boolean canTakeFromSlot = targetSlot.getStack().isEmpty() || targetSlot.canTakeStack(player);
-        final boolean bothSlotsEmpty = offHandStack.isEmpty() && targetSlot.getStack().isEmpty();
+        final ItemStack offHandStack = player.inventory.getStackInSlot(OFF_HAND_SLOT_INDEX);
+        final ItemStack slotStack = targetSlot.getStack();
+        if (offHandStack.isEmpty() && slotStack.isEmpty()) { return; }
+
+        final int maxItemsAllowed = targetSlot.getItemStackLimit(offHandStack);
+
+        // -- NOTE --
+        // When in creative mode, the player open container is {@link net.minecraft.client.gui.inventory.GuiContainerCreative$ContainerCreative}
+        // and the slots on the player regular inventory are converted to {@link net.minecraft.client.gui.inventory.GuiContainerCreative$CreativeSlot}.
+        //
+        // The slots on the creative tabs are basic instances of {@link net.minecraft.inventory.Slot}.
+        // Slots of the creative inventory have an InventoryBasic reference, player slots have a InventoryPlayer reference.
+
+        final boolean isPlayerOnCreativeInventory = player.openContainer.getClass().getName().contains(CREATIVE_CONTAINER_NAME);
+        final boolean isTargetSlotFromCreativeTabs = isPlayerOnCreativeInventory && !(targetSlot.inventory instanceof InventoryPlayer);
 
 
-        if (canPlaceOnSlot && canTakeFromSlot && !bothSlotsEmpty) {
 
-            // -- NOTE --
-            // When in creative mode, the player open container is {@link net.minecraft.client.gui.inventory.GuiContainerCreative$ContainerCreative}
-            // and the slots on the player regular inventory are converted to {@link net.minecraft.client.gui.inventory.GuiContainerCreative$CreativeSlot}.
-            //
-            // The slots on the creative tabs are basic instances of {@link net.minecraft.inventory.Slot}.
-            // Slots of the creative inventory have an InventoryBasic reference, player slots have a InventoryPlayer reference.
-
-            final boolean isPlayerOnCreativeInventory = player.openContainer.getClass().getName().contains(CREATIVE_CONTAINER_NAME);
-            final boolean isTargetSlotFromCreativeTabs = isPlayerOnCreativeInventory && !(targetSlot.inventory instanceof InventoryPlayer);
-
-
-            if (isTargetSlotFromCreativeTabs && offHandStack.isEmpty()) {
-                // Clones a full stack from the creative menu
+        if (isTargetSlotFromCreativeTabs) {
+            if (offHandStack.isEmpty()) {
+                // --- Clones a full stack from the creative menu
                 final ItemStack clonedStack = targetSlot.getStack().copy();
                 clonedStack.setCount(clonedStack.getMaxStackSize());
-                offHandSlot.putStack(clonedStack);
-
-            } else if (!isTargetSlotFromCreativeTabs) {
-                final int maxItemsAllowed = targetSlot.getItemStackLimit(offHandStack);
-
-                if (offHandStack.getCount() <= maxItemsAllowed) {
-                    // Swaps the item on the off hand slot with the target slot
-                    offHandSlot.putStack(targetSlot.getStack());
-                    targetSlot.putStack(offHandStack);
-                    targetSlot.onTake(player, offHandSlot.getStack());
-                    this._needsServerSide = true;
-
-                } else {
-                    // This target slot has limited capacity, only places a few items, if the slot is empty.
-                    if (targetSlot.getStack().isEmpty()) {
-                        targetSlot.putStack(offHandSlot.decrStackSize(maxItemsAllowed));
-                        targetSlot.onSlotChanged();
-                        this._needsServerSide = true;
-                    }
-
-                }
+                player.inventory.setInventorySlotContents(OFF_HAND_SLOT_INDEX, clonedStack);
             }
 
-        }
+
+        } else {
+
+            this._needsServerSide = true;
+
+            /**
+             * Reference: {@link net.minecraft.inventory.Container#slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player),
+             * where ClickType == ClickType.SWAP and dragType == 0 to 9, the hotbar slot}
+             */
+
+            if (offHandStack.isEmpty() && targetSlot.canTakeStack(player)) {
+                // --- Places item on the off-hand slot
+                player.inventory.setInventorySlotContents(OFF_HAND_SLOT_INDEX, slotStack);
+                // targetSlot.onSwapCraft(slotStack.getCount()); // Not visible, only used in SlotCrafting
+                targetSlot.putStack(ItemStack.EMPTY);
+                targetSlot.onTake(player, slotStack);
+
+
+            } else if (slotStack.isEmpty() && targetSlot.isItemValid(offHandStack)) {
+                // --- Places item on the target slot
+                if (offHandStack.getCount() > maxItemsAllowed) {
+                    targetSlot.putStack(offHandStack.splitStack(maxItemsAllowed));
+                } else {
+                    targetSlot.putStack(offHandStack);
+                    player.inventory.setInventorySlotContents(OFF_HAND_SLOT_INDEX, ItemStack.EMPTY);
+                }
+
+
+            } else if (targetSlot.canTakeStack(player) && targetSlot.isItemValid(offHandStack)) {
+                // --- Swap slots
+                if (offHandStack.getCount() > maxItemsAllowed) {
+                    targetSlot.putStack(offHandStack.splitStack(maxItemsAllowed));
+                    targetSlot.onTake(player, slotStack);
+                    if (!player.inventory.addItemStackToInventory(slotStack)) {
+                        player.dropItem(slotStack, true);
+                    }
+
+                } else {
+                    targetSlot.putStack(offHandStack);
+                    player.inventory.setInventorySlotContents(OFF_HAND_SLOT_INDEX, slotStack);
+                    targetSlot.onTake(player, slotStack);
+                }
+
+
+            }
+
+
+
+        } // if (isTargetSlotFromCreativeTabs)
+
 
     }
 
